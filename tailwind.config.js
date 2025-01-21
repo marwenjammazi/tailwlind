3e71a547b895649c2e435a594ada83eaf07f6301
Marwen Jammazi <marwenjammazi171@icloud.com>
2025-01-21 09:38:27 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        animation: {
+            'slow-spin-delayed': 'spin 3s linear 0.5s infinite',
+        },
+      },
+    },
+    plugins: [],
+  };
